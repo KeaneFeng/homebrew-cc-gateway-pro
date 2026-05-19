417e32adc266e7bedc0eab92da0687c9a1bdeb0f
Keane <503022048@qq.com>
2026-05-19 21:49:39 +0800

    Update cc-gateway-pro to v3.15.8

--- Casks/cc-gateway-pro.rb
+++ Casks/cc-gateway-pro.rb
@@ -1,8 +1,8 @@
 cask "cc-gateway-pro" do
-  version "3.15.5"
-  sha256 "9a67b77fd0aff9655eca3c5f7d74ac3ec5ca0710f5165de7964651fba3397007"
+  version "3.15.8"
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
 
-  url "https://github.com/KeaneFeng/cc-gateway-pro/releases/download/v#{version}/CC-Gateway-Pro_3.15.5_aarch64.dmg"
+  url "https://github.com/KeaneFeng/cc-gateway-pro/releases/download/v#{version}/CC-Gateway-Pro_3.15.8_aarch64.dmg"
   name "CC Gateway Pro"
   desc "Multi-provider aggregation gateway for Claude Code"
   homepage "https://github.com/KeaneFeng/cc-gateway-pro"
